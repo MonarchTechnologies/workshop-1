6d4d7a037a1bae8b2eaafc744bbb89ce17f40ee5
trystanmeadows <trystan@arroyodev.com>
2020-10-23 01:04:26 -0500

    Finally Fixed Problems. It was User Error. We defined Models using 'def' instead of 'class'. everything fixed now, its 1am, Im going to bed - A very Tired Trystan.

--- RiseManagementSoftware/RiseApp/models.py
+++ RiseManagementSoftware/RiseApp/models.py
@@ -2,30 +2,34 @@ from django.db import models
 
 # Create your models here.
 
-def Projects(models.Model):
-  description = models.TextField(blank=True, null=True)
-  projectTitle = models.CharField(max_length=13)
-  StartDate = models.DateTimeField('StartDate')
-  EndDate = models.DateTimeField('EndDate')
-  
-def Tasks(models.Model, models.ForeignKey(Projects, on_delete=models.CASCADE)):
-  task_name = models.CharField(max_length=13)
-  task_ description = models.TextField(blank=True, null=True)
-  created_date = models.DateTimeField('created_date')
-  finished_date = models.DateTimeField('finished_date')
+class Project(models.Model):
+    description = models.TextField(blank=True, null=True)
+    projectTitle = models.CharField(max_length=13)
+    StartDate = models.DateTimeField('StartDate')
+    EndDate = models.DateTimeField('EndDate')
 
-def Timer():
-  StartTime = models.TimeField(auto_now = false,auto_now_add=true)
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    task_name = models.CharField(max_length=13)
+    task_description = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField('created_date')
+    finished_date = models.DateTimeField('finished_date')
 
-  EndTime = models.TimeField(auto_now = true, auto_now_add = false)
 
-  TotalTime = models.TimeField()
+class Timer(models.Model):
+    StartTime = models.TimeField(auto_now_add='true')
+    EndTime = models.TimeField(auto_now='true')
+    TotalTime = models.TimeField(default='true')
 
-  #TotalTime = EndTime - StartTime
-def Teams():
-  TeamName = models.CharField(max_length=20)
-  SizeOfTeam = models.IntegerField()
-  TeamMembers = models.ListCharField(base_field=CharField(max_length=30),size = SizeOfTeam, max_length=(SizeOfTeam * 5))
-
-  #When adding new team members, code : object.TeamMembers.append
-# 
+class Team(models.Model):
+    TeamName = models.CharField(max_length=20)
+    SizeOfTeam = models.IntegerField()
+    TeamMembersList = []
+    TeamMember = models.CharField(max_length=20)
+    
+    # def AddTeamMember(TeamMember):
+    #     TeamMembersList.append(TeamMember)
+    # def DeleteTeamMember(TeamMember):
+    #     TeamMembersList.remove(TeamMember)
+  # When adding new team members, code : object.TeamMembers.append 
+  # Make Function in Views for Adding and Removing Team Members
