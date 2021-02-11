ac66feee231c9616925e107a188282a5c3471cdf
trystanmeadows <trystan@arroyodev.com>
2021-02-11 11:50:28 -0600

    Major Update: Integrated and Configured React , Webpack and Project dependencies.

--- RiseManagementSoftware/RiseApp_Teams/api.py
+++ RiseManagementSoftware/RiseApp_Teams/api.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import generics, permissions
 from .models import Team
 from .serializers import TeamSerializer
 # Create your views here.
